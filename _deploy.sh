b9f1057443ede63d89ea8ba13b5970bb33866542
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2024-06-25 07:24:38 +0200

    added script

---
+++ _deploy.sh
@@ -0,0 +1,12 @@
+# In Plesk, deploy with bash _deploy.sh >> deployment.log 2>&1
+
+echo ----------
+
+echo Copying PHP file 
+
+### Copy website
+cp index.php ~/data/index.php
+
+#cp -RT index.php ~/data/index.php
+
+echo ----------
